78dc4d4f3c8841db8205a93b3319de0185c0d833
Max Ebbing <max.k.ebbing@gmail.com>
2026-05-14 20:31:23 +0200

    Domain mistake

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,6 +1,6 @@
 import type { MetadataRoute } from "next";
 
-const baseUrl = "https://deinedomain.de";
+const baseUrl = "https://kebbing.com";
 
 export default function sitemap(): MetadataRoute.Sitemap {
   const routes = ["", "/kontakt", "/impressum", "/datenschutz"];
