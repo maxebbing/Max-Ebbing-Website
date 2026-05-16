483858eadb56372d7771a0788216a5a5e3e76072
Max Ebbing <max.k.ebbing@gmail.com>
2026-05-16 18:36:04 +0200

    Socials and Name-fix

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,10 +1,33 @@
+import { FaGithub, FaLinkedin } from "react-icons/fa";
 import styles from "./css/Footer.module.css";
 
 export default function Footer() {
   return (
     <footer className={styles.siteFooter}>
       <div className={`container ${styles.footerInner}`}>
-        <p>© 2026 Max Ebbing. Alle Rechte vorbehalten.</p>
+        <p>© 2026 Max Ebbing. Berlin.</p>
+
+        <div className={styles.socialLinks}>
+
+          <a
+            href="https://www.linkedin.com/in/max-ebbing-484989401/"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="LinkedIn"
+          >
+            <FaLinkedin size={18} />
+          </a>
+
+          <a
+            href="https://github.com/maxebbing"
+            target="_blank"
+            rel="noopener noreferrer"
+            aria-label="GitHub"
+          >
+            <FaGithub size={18} />
+          </a>
+
+        </div>
 
         <div className={styles.footerLinks}>
           <a href="/impressum">Impressum</a>
