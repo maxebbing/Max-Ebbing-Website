a1b5d0a858595d0596f4f807bf5d8c1ece9d2e7e
Max Ebbing <max.k.ebbing@gmail.com>
2026-05-18 00:00:12 +0200

    Vercel Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import "./globals.css";
 import CookieConsent from "@/components/Cookie";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   metadataBase: new URL("https://maxebbing.de"),
@@ -63,6 +64,7 @@ export default function RootLayout({
       <body>
         {children}
         <CookieConsent />
+        <Analytics />
       </body>
     </html>
   );
